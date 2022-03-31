3a47fd3372196b9c38f3eef2bb52284f1bd06a0b
Harm Meijer <harm.meijer@commercetools.com>
2022-03-31 09:06:29 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { Outlet } from "react-router-dom";
-import Menu from "./components/Menu";
+import { Link, Outlet } from 'react-router-dom';
+import Menu from './components/Menu';
+import './App.css';
 function App() {
   return (
     <div>
       <h1>App</h1>
       <nav
         style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
         }}
       >
         <Menu />
+        <div>
+          <Link to="/login">login</Link>
+        </div>
       </nav>
       <Outlet />
     </div>
